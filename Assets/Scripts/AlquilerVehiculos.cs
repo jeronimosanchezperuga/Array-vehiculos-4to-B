a06b577eedfd2e44fda6c100744d5a93119ec515
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2024-05-30 15:51:25 -0300

    Algunos ejercicios

---
+++ Assets/Scripts/AlquilerVehiculos.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlquilerVehiculos : MonoBehaviour
+{
+    public GameObject[] vehiculos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ResetearVehiculos();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            //activar solo un vehiculo al azar del array
+            int randomNumber = Random.Range(0,vehiculos.Length);
+            DesactivarTodosLosVehiculos();
+            vehiculos[randomNumber].SetActive(true);
+        }   
+    }
+
+    void ResetearVehiculos()
+    {
+        //desactivar todos los vehículos
+        DesactivarTodosLosVehiculos();
+        //y activar el primer elemento del array
+        vehiculos[0].SetActive(true);
+    }
+
+    void DesactivarTodosLosVehiculos()
+    {
+        for (int i = 0; i< vehiculos.Length; i++)
+        {
+            vehiculos[i].SetActive(false);
+        }
+    }
+}
